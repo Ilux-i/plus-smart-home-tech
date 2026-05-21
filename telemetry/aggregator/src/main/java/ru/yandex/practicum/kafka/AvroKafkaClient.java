975c2c33d181b15599cfe2c7f6b766fda94396c8
Ilux-i <iluxeo6@gmail.com>
2026-05-21 09:48:41 +0300

    Разработка агрегатора
    Добавлен сервис агрегатора
    Также разработана логика работы со снапшотами

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/kafka/AvroKafkaClient.java
@@ -0,0 +1,90 @@
+package ru.yandex.practicum.kafka;
+
+import jakarta.annotation.PreDestroy;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.MessageListener;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class AvroKafkaClient implements KafkaClient {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.consumer.group-id:avro-consumer-group}")
+    private String groupId;
+
+    private KafkaTemplate<String, byte[]> producer;
+    private Consumer<String, byte[]> consumer;
+
+    @Override
+    public KafkaTemplate<String, byte[]> getProducer() {
+        if (producer == null) {
+            producer = new KafkaTemplate<>(
+                    new DefaultKafkaProducerFactory<>(createProducerProps())
+            );
+        }
+        return producer;
+    }
+
+
+    public Consumer<String, byte[]> getConsumer() {
+        return new KafkaConsumer<>(createConsumerProps());
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        if (producer != null) {
+            try {
+                producer.flush();
+            } catch (Exception e) {
+                System.err.println("Ошибка очистки producer: " + e.getMessage());
+            }
+        }
+        if (consumer != null) {
+            try {
+                consumer.close();
+            } catch (Exception e) {
+                System.err.println("Ошибка закрытия consumer: " + e.getMessage());
+            }
+        }
+    }
+
+    // Producer конфигурация
+    private Map<String, Object> createProducerProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class);
+        return props;
+    }
+
+    // Consumer конфигурация
+    private Map<String, Object> createConsumerProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        return props;
+    }
+}
