7454c3c3dd2ee01c91c3eea172e5f00d47a1b6e7
Ilux-i <iluxeo6@gmail.com>
2026-05-23 14:55:48 +0300

    fix

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/kafka/AvroKafkaClient.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/kafka/AvroKafkaClient.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.kafka;
 
 import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -17,13 +18,14 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Component
 public class AvroKafkaClient implements KafkaClient {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    @Value("${spring.kafka.consumer.group-id:avro-consumer-group}")
+    @Value("${spring.kafka.consumer.group-id:analyzer-group}")
     private String groupId;
 
     private KafkaTemplate<String, byte[]> producer;
@@ -39,7 +41,6 @@ public class AvroKafkaClient implements KafkaClient {
         return producer;
     }
 
-
     public Consumer<String, byte[]> getConsumer() {
         return new KafkaConsumer<>(createConsumerProps());
     }
@@ -49,38 +50,42 @@ public class AvroKafkaClient implements KafkaClient {
         if (producer != null) {
             try {
                 producer.flush();
+                log.info("Kafka producer успешно очищен");
             } catch (Exception e) {
-                System.err.println("Ошибка очистки producer: " + e.getMessage());
+                log.error("Ошибка очистки producer: {}", e.getMessage(), e);
             }
         }
         if (consumer != null) {
             try {
                 consumer.close();
+                log.info("Kafka consumer успешно закрыт");
             } catch (Exception e) {
-                System.err.println("Ошибка закрытия consumer: " + e.getMessage());
+                log.error("Ошибка закрытия consumer: {}", e.getMessage(), e);
             }
         }
     }
 
-    // Producer конфигурация
     private Map<String, Object> createProducerProps() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class);
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 3);
         return props;
     }
 
-    // Consumer конфигурация
     private Map<String, Object> createConsumerProps() {
         Map<String, Object> props = new HashMap<>();
+
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
+
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 600_000);
+        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 300_000);
         props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1);
         return props;
     }
